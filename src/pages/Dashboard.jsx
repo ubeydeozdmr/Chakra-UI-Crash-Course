e10e3cd1ad6fc6c79e2abcd3030afaf41cb842b3
Ubeyde Emir Özdemir <ubeydeozdmr@gmail.com>
2024-03-02 18:42:50 +0300

    Components & Style Props

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,3 +1,38 @@
+import { Container, Heading, Text, Box } from '@chakra-ui/react';
+
 export default function Dashboard() {
-  return <div>Dashboard</div>;
+  const boxStyles = {
+    p: '10px',
+    bg: 'purple.400',
+    color: 'white',
+    m: '10px',
+    textAlign: 'center',
+    filter: 'blur(2px)',
+    '&:hover': {
+      color: 'black',
+      bg: 'blue.200',
+    },
+  };
+
+  return (
+    <Container as="section" maxWidth="4xl" py="20px">
+      <Heading my="30px" p="10px">
+        Chakra UI Components
+      </Heading>
+      <Text marginLeft="30px">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Rem,
+        laudantium?
+      </Text>
+      <Text ml="30px" color="blue.300" fontWeight="bold">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Rem,
+        laudantium?
+      </Text>
+
+      <Box my="30px" p="20px" bg="orange">
+        <Text color="white">This is a box</Text>
+      </Box>
+
+      <Box sx={boxStyles}>Hello, Ninjas!</Box>
+    </Container>
+  );
 }
