1e737e5454c92f2f11d4cbfb127bcca0505ea02a
Ubeyde Emir Özdemir <ubeydeozdmr@gmail.com>
2024-03-02 19:57:16 +0300

    Grid Layouts

--- src/layouts/RootLayout.jsx
+++ src/layouts/RootLayout.jsx
@@ -1,11 +1,23 @@
+import { Grid, GridItem } from '@chakra-ui/react';
 import { Outlet } from 'react-router-dom';
 import Navbar from '../components/Navbar';
 
 export default function RootLayout() {
   return (
-    <div>
-      <Navbar />
-      <Outlet />
-    </div>
+    <Grid templateColumns="repeat(6, 1fr)" bg="gray.50">
+      <GridItem
+        as="aside"
+        colSpan="1"
+        bg="purple.400"
+        minHeight="100vh"
+        p="30px"
+      >
+        <span>sidebar</span>
+      </GridItem>
+      <GridItem as="main" colSpan="5" p="40px">
+        <Navbar />
+        <Outlet />
+      </GridItem>
+    </Grid>
   );
 }
--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,38 +1,22 @@
-import { Container, Heading, Text, Box } from '@chakra-ui/react';
+import { Box, SimpleGrid } from '@chakra-ui/react';
 
 export default function Dashboard() {
-  const boxStyles = {
-    p: '10px',
-    bg: 'purple.400',
-    color: 'white',
-    m: '10px',
-    textAlign: 'center',
-    filter: 'blur(2px)',
-    '&:hover': {
-      color: 'black',
-      bg: 'blue.200',
-    },
-  };
-
   return (
-    <Container as="section" maxWidth="4xl" py="20px">
-      <Heading my="30px" p="10px">
-        Chakra UI Components
-      </Heading>
-      <Text marginLeft="30px">
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Rem,
-        laudantium?
-      </Text>
-      <Text ml="30px" color="blue.300" fontWeight="bold">
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Rem,
-        laudantium?
-      </Text>
+    <SimpleGrid p="10px" spacing={10} minChildWidth="250px">
+      <Box bg="white" h="200px" border="1px solid"></Box>
+      <Box bg="white" h="200px" border="1px solid"></Box>
+      <Box bg="white" h="200px" border="1px solid"></Box>
+      <Box bg="white" h="200px" border="1px solid"></Box>
 
-      <Box my="30px" p="20px" bg="orange">
-        <Text color="white">This is a box</Text>
-      </Box>
+      <Box bg="white" h="200px" border="1px solid"></Box>
+      <Box bg="white" h="200px" border="1px solid"></Box>
+      <Box bg="white" h="200px" border="1px solid"></Box>
+      <Box bg="white" h="200px" border="1px solid"></Box>
 
-      <Box sx={boxStyles}>Hello, Ninjas!</Box>
-    </Container>
+      <Box bg="white" h="200px" border="1px solid"></Box>
+      <Box bg="white" h="200px" border="1px solid"></Box>
+      <Box bg="white" h="200px" border="1px solid"></Box>
+      <Box bg="white" h="200px" border="1px solid"></Box>
+    </SimpleGrid>
   );
 }
