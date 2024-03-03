d12f63a4a35cca485c3b0611c75d9d7f8f648770
Ubeyde Emir Özdemir <ubeydeozdmr@gmail.com>
2024-03-03 15:15:30 +0300

    Customizing the Theme

--- src/layouts/RootLayout.jsx
+++ src/layouts/RootLayout.jsx
@@ -9,7 +9,7 @@ export default function RootLayout() {
       <GridItem
         as="aside"
         colSpan={{ base: 6, lg: 2, xl: 1 }}
-        bg="purple.400"
+        bg="purple.400" // brand.600
         minHeight={{ lg: '100vh' }}
         p={{ base: '20px', lg: '30px' }}
       >
--- src/main.jsx
+++ src/main.jsx
@@ -2,11 +2,30 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 
-import { ChakraProvider } from '@chakra-ui/react';
+import { ChakraProvider, extendTheme } from '@chakra-ui/react';
+
+// extend the theme
+// const colors = {
+//   brand: {
+//     900: '#024fc9',
+//     800: '#146af5',
+//     700: '#2977f2',
+//     600: '#337df2',
+//     500: '#4287f5',
+//   },
+// };
+
+// const fonts = {
+//   body: 'Tahoma, sans-serif',
+//   heading: 'Courier New, monospace',
+// };
+
+// const theme = extendTheme({ colors, fonts });
+const theme = extendTheme({});
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <ChakraProvider>
+    <ChakraProvider theme={theme}>
       <App />
     </ChakraProvider>
   </React.StrictMode>,
