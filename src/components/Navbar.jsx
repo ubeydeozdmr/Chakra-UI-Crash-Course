fd0621bdb3b91c8f8d627e3d053c2311f99d0cca
Ubeyde Emir Özdemir <ubeydeozdmr@gmail.com>
2024-03-03 14:59:43 +0300

    Toast Component

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,3 +1,4 @@
+import { UnlockIcon } from '@chakra-ui/icons';
 import {
   Flex,
   Box,
@@ -6,9 +7,24 @@ import {
   Button,
   Spacer,
   HStack,
+  useToast,
 } from '@chakra-ui/react';
 
 function Navbar() {
+  const toast = useToast();
+
+  const showToast = () => {
+    toast({
+      title: 'Logged out',
+      description: 'Successfully logged out',
+      duration: 5000,
+      isClosable: true,
+      status: 'success',
+      position: 'top',
+      icon: <UnlockIcon />,
+    });
+  };
+
   return (
     <Flex as="nav" p="10px" mb="40px" alignItems="center">
       <Heading as="h1">Dojo Tasks</Heading>
@@ -19,7 +35,9 @@ function Navbar() {
           M
         </Box>
         <Text>mario@netninja.dev</Text>
-        <Button colorScheme="purple">Logout</Button>
+        <Button colorScheme="purple" onClick={showToast}>
+          Logout
+        </Button>
       </HStack>
     </Flex>
   );
