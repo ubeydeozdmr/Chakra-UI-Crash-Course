21c2f9743cd24e2885dd33273c36d620782b09b5
Ubeyde Emir Özdemir <ubeydeozdmr@gmail.com>
2024-03-03 11:08:19 +0300

    Lists & List Icons

---
+++ src/components/Sidebar.jsx
@@ -0,0 +1,30 @@
+import { AtSignIcon, CalendarIcon, EditIcon } from '@chakra-ui/icons';
+import { List, ListIcon, ListItem } from '@chakra-ui/react';
+import { NavLink } from 'react-router-dom';
+
+function Sidebar() {
+  return (
+    <List color="white" fontSize="1.2em" spacing={4}>
+      <ListItem>
+        <NavLink to="/" exact>
+          <ListIcon as={CalendarIcon} color="white" />
+          Dashboard
+        </NavLink>
+      </ListItem>
+      <ListItem>
+        <NavLink to="create">
+          <ListIcon as={EditIcon} color="white" />
+          Create
+        </NavLink>
+      </ListItem>
+      <ListItem>
+        <NavLink to="profile">
+          <ListIcon as={AtSignIcon} color="white" />
+          Profile
+        </NavLink>
+      </ListItem>
+    </List>
+  );
+}
+
+export default Sidebar;
